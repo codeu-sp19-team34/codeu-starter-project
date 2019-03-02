e24e6aed314ccff741cc6135f403d6c7be61ab64
HL3rd <hlopez3rd@gmail.com>
2019-03-02 10:43:11 -0800

    Continuing debug
    Loading... string still appearing

--- src/main/webapp/js/stats-page-loader.js
+++ src/main/webapp/js/stats-page-loader.js
@@ -23,12 +23,12 @@ function fetchStats(){
 }
 
 function buildStatElement(statString){
- const statElement = document.createElement('p');
- statElement.appendChild(document.createTextNode(statString));
- return statElement;
+  const statElement = document.createElement('p');
+  statElement.appendChild(document.createTextNode(statString));
+  return statElement;
 }
 
 /** Fetch data and populate the UI of the page. **/
 function buildUI(){
- fetchStats();
+  fetchStats();
 }
