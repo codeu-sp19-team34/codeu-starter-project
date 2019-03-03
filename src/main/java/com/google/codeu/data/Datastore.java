5c8f7c1f47f6b92e332d23b6bc1a6e7545935ec1
sebrianneferguson <42628590+sebrianneferguson@users.noreply.github.com>
2019-03-02 20:13:12 -0800

    removed unneccesary recipient line

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -107,7 +107,6 @@ public class Datastore {
         String user = (String) entity.getProperty("user");
         String text = (String) entity.getProperty("text");
         long timestamp = (long) entity.getProperty("timestamp");
-        String recipient = (String) entity.getProperty("recipient");
 
         Message message = new Message(id, user, text, timestamp, recipient);
         messages.add(message);
